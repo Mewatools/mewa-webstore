1743f6af41e005dd94d1d60e0428124be6d1ecea
Songtech-0912 <dixinlily@hotmail.com>
2021-05-04 18:39:42 -0400

    Added optional responsive debugging code to index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // Google tag manager supporting script
-window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || []
 function gtag() {
-	dataLayer.push(arguments);
+  dataLayer.push(arguments)
 }
-gtag("js", new Date());
-gtag("config", "UA-3015459-1");
+gtag('js', new Date())
+gtag('config', 'UA-3015459-1')
+
+// For responsive debugging purposes, comment out in production
+// window.addEventListener('resize', function() {
+//   console.log(window.innerWidth + "px", window.innerHeight + "px");
+// });
